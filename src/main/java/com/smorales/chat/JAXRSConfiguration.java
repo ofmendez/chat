32430f18fb2d6839339b205d71982e74892ee183
Ofmendez(wise) <ofmendez@gmail.com>
2016-09-29 15:40:22 -0500

    Agregado el JAXRSConfiguration Signed-off-by: Ofmendez(wise) <ofmendez@gmail.com>

---
+++ src/main/java/com/smorales/chat/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package com.smorales.chat;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
